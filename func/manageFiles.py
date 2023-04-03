c6b4bd5e87028e1ec679690f7b92a83a9173beb6
Anuja Nimesh <anujanimesh@gmail.com>
2023-04-03 19:58:45 +0530

    fixed file managing issue

--- func/manageFiles.py
+++ func/manageFiles.py
@@ -1,14 +1,14 @@
 import os
 
 def md(paths):
-    paths = [paths] if isinstance(paths, list) else paths
+    paths = path if isinstance(paths, list) else [paths]
     for path in paths:
         if not os.path.isdir(path):
             os.mkdir(path)
 
 
 def mkfile(paths):
-    paths = [paths] if isinstance(paths, list) else paths
+    paths = path if isinstance(paths, list) else [paths]
     for path in paths:
         if not os.path.exists(path):
             open(path, "w").close()
