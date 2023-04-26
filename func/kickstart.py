8b97c92097ba8c8a7e5524a9c7bf760b90d200bf
Anuja Nimesh <anujanimesh@gmail.com>
2023-04-27 02:44:22 +0530

    fix(kickstart) uses recursive function instead of while loop

--- func/kickstart.py
+++ func/kickstart.py
@@ -1,37 +1,41 @@
 import re
-import time
 from collections import OrderedDict
 
 from .downloader import downloader
 from .manageData import appendData, readData
 
+NEED_LOC = "./_need.txt"
+DONE_LOC = "./_done.txt"
+
 
 def confirmUrl(url):
-    url=url.replace('\n', '')
+    url = url.replace('\n', '')
     if len(re.findall(r"sarigama\.lk\/artist\/.+\/.+", url)) < 1:
         return f"Unsupported link:\n{url}\n"
 
     url = "https://"+url if len(re.findall(r"(https?:\/\/)", url)) < 1 else url.replace('http://', 'https://')
     return url
 
+
+def readNeeds():
+    return list(OrderedDict.fromkeys((readData(NEED_LOC))))
+
+
+def removeFirstLine(loc):
+    with open(loc, 'r') as fin:
+        data = fin.read().splitlines(True)
+    with open(loc, 'w') as fout:
+        fout.writelines(data[1:])
+
+
 def kickstart(downloadsloc, dataloc):
-    needsLoc = "./_need.txt"
-    needs = []
-    if len((newNeeds := readData(needsLoc))) == 0:
-        print("Instructions: To start downloading put artist links in `_need.txt`.\nEach link in a seperate line")
-        time.sleep(3)
-    while needs != (newNeeds := readData(needsLoc)):
-        needs = list(OrderedDict.fromkeys(newNeeds))
-        for url in needs:
-            url = confirmUrl(url)
-            if "Unsupported" in url:
-                print(url)
-                continue
-            if (res := downloader(url, downloadsloc, dataloc)) == True:
-                with open(needsLoc, 'r') as fin:
-                    data = fin.read().splitlines(True)
-                with open(needsLoc, 'w') as fout:
-                    fout.writelines(data[1:])
-                appendData('./_done.txt', url)
-            else:
-                print(res)
+    for url in (needs := readNeeds()):
+        if "Unsupported" in (url := confirmUrl(url)):
+            print(url)
+        elif (res := downloader(url, downloadsloc, dataloc)) == True:
+            removeFirstLine(NEED_LOC)
+            appendData(DONE_LOC, url)
+        else:
+            print(res)
+    if needs != readNeeds():
+        kickstart(downloadsloc, dataloc)
