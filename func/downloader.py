5aa2c6bb5bee45dddeeb741bfacb6eef2e8b776b
Anuja Nimesh <anujanimesh@gmail.com>
2023-04-26 23:59:52 +0530

    fix(concurrentDownloadCounter) changed to shorthand

--- func/downloader.py
+++ func/downloader.py
@@ -17,6 +17,7 @@ def download(title, url, filepath, dataLoc, cookies):
     if os.path.exists(filepath) and os.path.getsize(filepath) > 0:
         appendData(dataLoc, title)
         print(f"{title} was already downloaded.")
+
     try:
         fileres = requests.get(url, cookies=cookies, stream=True)
     except:
@@ -29,12 +30,8 @@ def download(title, url, filepath, dataLoc, cookies):
     appendData(dataLoc, title)
 
 
-def downloadingCount(artistLoc):
-    i = 0
-    for track in os.listdir(artistLoc):
-        if os.path.getsize(os.path.join(artistLoc, track)) == 0:
-            i += 1
-    return i
+def concurrentDownloadCounter(artistLoc):
+    return len([track for track in os.listdir(artistLoc) if os.path.getsize(os.path.join(artistLoc, track)) == 0])
 
 
 def downloader(url, downloadsloc, dataloc):
@@ -60,7 +57,7 @@ def downloader(url, downloadsloc, dataloc):
     md(os.path.join(artistLoc))
 
     for n, (url, songtitle) in enumerate(songurls):
-        while downloadingCount(artistLoc)+1 >= ALLOWED_CONCURRENT_DOWNLOADS:
+        while concurrentDownloadCounter(artistLoc)+1 >= ALLOWED_CONCURRENT_DOWNLOADS:
             time.sleep(1)
 
         downloadPageUrl = re.findall(r"<a target=\"_blank\" href=\"(https:\/\/sarigama.lk\/songs.+?)\"", requests.get(url).text)[0]
