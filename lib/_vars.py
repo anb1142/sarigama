51cfb0c30252b5d6e4a6035c9766aa2868df6bd5
Anuja Nimesh <anujanimesh@gmail.com>
2023-05-08 20:22:13 +0530

    feat(downloader) uses htmlparsing instead of regex

--- lib/_vars.py
+++ lib/_vars.py
@@ -6,3 +6,6 @@ DOWNLOAD_LOC = Path(PATH, '_downloads')
 DATA_LOC = Path(PATH, '_data')
 DONE_LOC = Path(PATH, '_done.txt')
 NEED_LOC = Path(PATH, '_need.txt')
+
+PARSER = 'html.parser'
+ALLOWED_CONCURRENT_DOWNLOADS = 3
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import os
 import sys
 
-from lib._vars import DATA_LOC, DOWNLOAD_LOC, DONE_LOC, NEED_LOC
+from lib._vars import DATA_LOC, DONE_LOC, DOWNLOAD_LOC, NEED_LOC
 from lib.isfinished import isfinished
 from lib.kickstart import kickstart
 from lib.manageData import append_data, read_data
 from lib.manageFiles import md, mkfile
-
-
 
 if __name__ == "__main__":
     md([DOWNLOAD_LOC, DATA_LOC])
